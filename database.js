73f3e40ea2a6218112e695bd3c2b1de9ad9779f5
assan1997 <49047175+assan1997@users.noreply.github.com>
2022-03-29 16:13:48 +0000

    ok 16h30

--- database.js
+++ database.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql');
+require('dotenv').config();
 
 const dbConnect = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'educdb'
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
 });
 
 module.exports = dbConnect;
